a9de926b705290f64d6dc22a74e03df9fad6bed1
Stefan Thesing <software@webdings.de>
2020-04-01 15:57:41 +0200

    Updated to TA 10, diverse  Lexer, Themes and Module Stuff, Snippets

--- init.lua
+++ init.lua
@@ -1,13 +1,35 @@
-
-
--- Theme
-if not CURSES then 
-    ui.set_theme('base16-solarized-dark', 
-    {font = 'Inconsolata', fontsize = 12}) 
+---------------------------------
+-- Theme and Buffer Settings   --
+---------------------------------
+if CURSES then
+    buffer:set_theme('term')
+    --buffer.caret_line_back = 0xff0000
+    buffer.caret_line_back = buffer.property['color.light_black']
+    buffer.caret_line_visible = true
+    buffer.edge_colour = 0x00FFFF
+    --buffer.edge_colour = buffer.property['color.light_blue']
 end
 
--- Snippets
-require("snippets/snippets")
+if not CURSES then 
+    buffer:set_theme('base16-solarized-dark',
+    --{fontsize = 13}
+    --{font = 'Inconsolata', fontsize = 14}
+    --{font = 'Monaco', fontsize = 12}
+    {font = 'Noto Mono', fontsize = 12}
+    --{font = 'Atari ST 8x16 System Font', fontsize = 14}
+    --{font = 'Ubuntu Mono', fontsize = 14}
+    --{font = 'Roboto Mono', fontsize = 12}
+    )
+    buffer.edge_colour = buffer.property_int["color.base0A"]
+    buffer.zoom = 2
+end
+
+buffer.use_tabs = false
+buffer.tab_width = 4
+buffer.wrap_mode = buffer.WRAP_WORD
+buffer.edge_column = 79
+buffer.edge_mode = buffer.EDGE_LINE
+
 
 -- Elastic Tabstops
 require('elastic_tabstops').enable()
@@ -19,17 +41,25 @@ yaml = require "yaml"
 --lyaml = require "yaml.lyaml"
 
 -- Zettels
+---[[
 if not CURSES then
     local zettels = require('zettels')
-    zettels.enable(os.getenv("HOME") .. '/Dokumente/Zettelkasten/', os.getenv("HOME") .. "/.config/Zettels/index.yaml")
+    zettels.enable(os.getenv("HOME") .. '/space/Dokumente/Wissenschaft/zettelkasten/', os.getenv("HOME") .. "/.config/Zettels/index.yaml")
+    
+    keys['az'] = {
+        t = function() zettels.search_zettel('Title') end,
+        k = function() zettels.search_zettel('Tags') end,
+        n = function() zettels.search_zettel('File') end,
+        f = function() zettels.search_followups(buffer.filename) end,
+    }
 end
+--]]
 
--- Keyboard Chains
-keys['c2'] = {
-    -- German Quotation marks
-    up = function() buffer.add_text("“") end,
-    down = function() buffer.add_text("„") end
-}
+--------------------
+--    ta-rust     --  
+--------------------
+_RACER   = false
+_RUSTFMT = false
 
 -----------------------
 -- Language settings --
@@ -51,3 +81,25 @@ events.connect(events.LEXER_LOADED, function(lexer)
     buffer.view_ws = buffer.WS_VISIBLEONLYININDENT
   end
 end)
+
+-- Recognize .ino files as cpp code.
+textadept.file_types.extensions.ino = 'cpp'
+-- Recognize .toml files as TOML.
+textadept.file_types.extensions.toml = 'toml'
+
+-----------------------------------
+-- Personalized compile & run commands --
+-----------------------------------
+-- LATEX
+textadept.run.compile_commands.tex='latexmk -outdir=render -silent -xelatex "%f"'
+textadept.run.run_commands.tex='xdg-open "render/%e.pdf"'
+
+-- RUST
+-- For the compile command, I use cargo's "check"-functionality instead of 
+-- build. It's faster, and if I want to actually build the program, I usually
+-- also want to run it.
+textadept.run.compile_commands.rust='cargo check --manifest-path ../Cargo.toml'
+textadept.run.run_commands.rust='cargo run --manifest-path ../Cargo.toml'
+
+-- Snippets
+require("snippets/snippets")
--- snippets/snippets.lua
+++ snippets/snippets.lua
@@ -3,6 +3,8 @@
 Textadept stores its built-in snippets in the global variable "snippets".
 I don't want to erase those. Instead I want to _add_ to them, overwriting 
 defaults where necessary.
+
+Also here (scroll down): Key Chains
 --]]
 
 -- a function to insert user defined snippets into the global snippets
@@ -20,12 +22,14 @@ require("snippets/dynamic-snippets")
 --- ##########################################################################
 usersnippets = {
     ['file'] = '%<buffer.filename>',
+    ['today'] = os.date("%Y-%m-%d", os.time()),
+
     -- A snippet to speed up the notation of snippets (Mind. Blown.)
     ['sn']      = '[\'%1(trigger)\']\t\t= \'%2(expanded)\',\n%0',
     ['fence']   = '-- ' .. string.rep('#', 75),
     ['ldots']   = '…',
     ['...']     = '…',
-    
+    ['commit']  = '%1(type)(%2(scope)): %3(subject)\n\n%4(body)\n\n%0(footer)',    
 
 -- Specific stuff I frequently use in my German academic texts    
     ['beh']     = 'Behindertenpädagogik',
@@ -36,6 +40,7 @@ usersnippets = {
     ['HO']      = 'Handlungsorientierung',
     ['ho']      = 'handlungsorientiert',
     ['Reha']    = 'Rehabilitation',
+    ['Part']    = 'Partizipation',
     ['anfuu']   = '„%0“',
     ['anfuo']   = '“',
     ['mmb']     = 'Menschen mit Behinderung',
@@ -56,8 +61,13 @@ usersnippets = {
     ['bbo']		= 'berufsbildorientiert',
     ['qg']		= 'Qualifizierung',
     ['qo']		= 'Qualifikation',
-      
-      
+    ['sdb']      = 'sonderpädagogischer Förderbedarf',  
+    ['fd']       = 'familienentlastender Dienst',
+    ['zizek']    = 'Žižek',
+    ['!=']       = '≠',
+    ['ungefähr'] = '≈',
+    ['entspricht'] = '≙',
+    ['Ant']      = 'Anthropologie',
 }
 
 -- Add user defined snippets to the preconfigured ones, overwriting, if 
@@ -67,20 +77,20 @@ merge_snippets(snippets, usersnippets)
 --- # Markdown Snippets
 snippets.markdown = {
 -- Headers.
-  ['1'] = '# ',
-  ['2'] = '## ',
-  ['3'] = '### ',
-  ['4'] = '#### ',
-  ['5'] = '##### ',
-  ['6'] = '###### ',
+  ['h1'] = '# ',
+  ['h2'] = '## ',
+  ['h3'] = '### ',
+  ['h4'] = '#### ',
+  ['h5'] = '##### ',
+  ['h6'] = '###### ',
 
 --Links
-  link = '[%1(Link)](%2(http://example.net/))',
+  link = '[%1(Link)](%0(http://example.net/))',
 --Clickable link.
-  clink = '<%1(http://example.com/)>',
+  clink = '<%0(http://example.com/)>',
 --Reference-style link.
-  rlink = '[%1(example)][%2(ref)]',
-  id = '[%1(ref)]: %2(http://example.com/)',
+  rlink = '[%1(example)][%0(ref)]',
+  id = '[%1(ref)]: %0(http://example.com/)',
 
 -- Code.
 	c = '`%0`',
@@ -89,12 +99,42 @@ snippets.markdown = {
     --python
     cbp = '```{.python .numberLines}\n%0\n```',
 -- Image.
-	i = '![%1(Alt text)](%2(/path/to/img.jpg "Optional title"))',
--- Zettel metadata
-    zet = "---\ntitle:  '%1(title)'\nauthor: Stefan Thesing\ntags: []\nfollowups: []\n...\n\n# %0(section1)",
+	img = '![%1(Alt text)](%2(/path/to/img.jpg "Optional title"))',
+-- Zettel boilerplate
+    zet = "---\ntitle:  '%1(title)'\nauthor: Stefan Thesing\ntags: [%2(tag)]\nfollowups: []\nlang: de\n...\n\n# %0(section1)\n\n\n[//]: # (Links)",
+    
 -- Changelog block
-    cl = "## [Unreleased]\n### Added\n### Changed\n%0\n### Deprecated\n### Removed\n### Fixed\n### Security\n"
-}
+    cl = "## [Unreleased]\n### Added\n### Changed\n%0\n### Deprecated\n### Removed\n### Fixed\n### Security\n",
+
+    ['cite'] = '[@%1(source) %2(page)]%0',
+    ['zit']  = '>„%1(Quote)“\n([@%2(source) %3(page)])%0',
+    ['vgl']  = '(vgl. [@%1(source) %2(page)])%0',
+    
+    -- Latex in Markdown (for Zettels)
+    ['enq']         = '\\enquote{%1}',
+    ['lcite']        = '\\cite[S.\\,%2(page)]{%1(source)}',
+    ['lzit']         = '\\begin{quote}\n\t\\enquote{%1(Quote)}\n\t\\parencite[S.\\,%3(page)]{%2(source)}\n\\end{quote}',
+    --- blockquotes for laws
+    ['rzit']        = '\\begin{quote}\n\t\\enquote{%1(Quote)}\n\t(§\\,%2(source))\n\\citereset{}\n\\end{quote}',
+    ['parencite']        = '\\parencite[S.\\,%2(page)]{%1(source)}',
+    ['textcite']        = '\\textcite[S.\\,%2(page)]{%1(source)}',   
+    --- \citeauthor{key}
+    ['aut']             = '\\citeauthor{%0}',
+    --- aut as a dynamic snippet
+    ['daut']            = '\\citeauthor{' .. dynsn.fields.current_author .. '} ',
+    --- \citetitle{key}
+    ['citetitle']       = '\\citetitle{%0}',
+    --- \citeyear [ prenote ][ postnote ]{ key }
+    ['citeyear']        = '\\citeyear{%0}',
+    --- \fullcite{ key}
+    ['citereset']       = '\\citereset{%0}',
+        --- one of them usually followed by a citation.
+    ['lvgl']             = '(vgl. \\cite[S.\\,%2(page)]{%1(source)})',
+        -- current_author ist defined in dynamic snippets. It's usually a bibtex key
+    ['ca']      = dynsn.fields.current_author,
+    -- semantic annotations; repurposing the fenced_div extenstion of pandoc
+    ['ann'] = "::: {#%1(kind) note=\"%2(note)\"}\n%0(annotated-content)\n:::\n",
+    }
 
 
 
@@ -116,16 +156,18 @@ snippets.latex = {
     --- first, generics
     ['begin']       = '\\begin{%1(type)}\n\t%3( )\n\\end{%1}\n%0',
     ['item']        = '\\item %0',
-    ['desitem']     = '\\item[%1(des)] %2(item)',
-    ['ditem']       = '\\item[%1(des)] %2(item)',
-    --- next, the classics
-    ['itemize']     = '\\begin{itemize}\n\t\\item %1(item)\n\\end{itemize}\n%0',
-    ['enum']        = '\\begin{enumerate}\n\t\\item %1(item)\n\\end{enumerate}\n%0',
-    ['enumerate']   = '\\begin{enumerate}\n\t\\item %1(item)\n\\end{enumerate}\n%0',
-    ['description'] = '\\begin{description}\n\t\\item[%1(des)] %2(item)\n\\end{description}\n%0',
-    ['des']         = '\\begin{description}\n\t\\item[%1(des)] %2(item)\n\\end{description}\n%0',
-    ['desindent']   = '\\begin{itemize}[style=multiline,leftmargin=2.5cm,font=\\normalfont]\n\t\\item[%1(des)] %2(item)\n\\end{itemize}\n%0',
-    ['frame']       = '\\begin{frame}\n\t\\item %1(content)\n\\end{frame}\n%0',
+    ['desitem']     = '\\item[%1(des)] %0(item)',
+    ['ditem']       = '\\item[%1(des)] %0(item)',
+    --- next, the classics, with a tightlist-option (see below)
+    ['itemize']     = '\\begin{itemize}%1(\\tightlist)\n\t\\item %0(item)\n\\end{itemize}\n',
+    ['enum']        = '\\begin{enumerate}%1(\\tightlist)\n\t\\item %0(item)\n\\end{enumerate}\n',
+    ['enumerate']   = '\\begin{enumerate}%1(\\tightlist)\n\t\\item %0(item)\n\\end{enumerate}\n',
+    ['description'] = '\\begin{description}%1(\\tightlist)\n\t\\item[%2(des)] %0(item)\n\\end{description}\n',
+    ['des'] = '\\begin{description}%1(\\tightlist)\n\t\\item[%2(des)] %0(item)\n\\end{description}\n',
+    -- the tightlist command from pandoc, definition for the document's preamble
+    ['tl']          = '\\providecommand{\\tightlist}{\\setlength{\\itemsep}{0pt}\\setlength{\\parskip}{0pt}}', 
+    -- For Beamer Presentations
+    ['frame']       = '\\begin{frame}{%1(title)}\n\t%0(content)\n\\end{frame}\n',
     --- skeleton of my most frequently used table
     ['table']       = '\\begin{table}[hb]\n\t\\begin{tabularx}{\\textwidth}{|l|X|}\n\t\\hline\n\t\\rowcolor{lightgray}\n\t%1(Header Column1) & %2(Header Column2)\\\\ \\hline\n\t%5(Row1Col1)& %6(Row1Col2)\\\\ \\hline\n\t%7(Row2Col1) & %8(Row2Col2)\\\\ \\hline\n\t\\end{tabularx}\n\t\\caption {%3(Caption)}\n\t\\label{tab:%4(label)}\n\\end{table}',
     -- Figure
@@ -145,27 +187,20 @@ snippets.latex = {
     ['page']        = '\\pageref{%0}',
     ['name']        = '\\nameref{%0}',
 -- Quoting, using csquotes
-    ['enq']         = '\\enquote{%1}%0',
+    ['enq']         = '\\enquote{%1}',
     --- blockquotes
-    ['zit']         = '\\begin{quote}\n\t\\enquote{%1(Quote)}\n\t\\parencite[S.\\,%3(page)]{%2(source)}\n\\end{quote}',
+    --['zit']         = '\\begin{quote}\n\t\\enquote{%1(Quote)}\n\t\\parencite[S.\\,%3(page)]{%2(source)}\n\\end{quote}',
+    ['zit']         = '\\begin{quote}\n\t\\enquote{%1(Quote)}\n\t\\parencite[%3(S_wenn_f._o._Anm.)]{%2(source)}\n\\end{quote}\n',
+    ['vgl']             = '(vgl. \\cite[%2(S_wenn_f._o._Anm.)]{%1(source)})',
+    
+    ['S'] = 'S.\\,%0\\,f.',
+    ['s'] = 'S.\\,%0\\,f.',
     --- blockquotes for laws
-    ['rzit']        = '\\begin{quote}\n\t\\enquote{%1(Quote)}\n\t(§\\,%2(source))\n\\citereset{}\n\\end{quote}',
+    ['rzit']        = '\\begin{quote}\n\t\\enquote{%1(Quote)}\n\t(§\\,%2(source))\n\\citereset{}\n\\end{quote}\n',
 -- Biblatex citation commands
-    --- \cite [ prenote ][ postnote ]{ key }
-    ['citepre']     = '\\cite[%3(prenote)][S.\\,%2(page){%1(source)}',
-    --- \cite [ postnote ]{ key }
-    ['cite']        = '\\cite[S.\\,%2(page)]{%1(source)}',
-    
-    --- \parencite [ prenote ][ postnote ]{ key }
-    ['parencitepre']     = '\\parencite[%3(prenote)][S.\\,%2(page){%1(source)}',
-    --- \parencite [ postnote ]{ key }
-    ['parencite']        = '\\parencite[S.\\,%2(page)]{%1(source)}',
-    
-    --- \textcite [ prenote ][ postnote ]{ key }
-    ['textcitepre']     = '\\textcite[%3(prenote)][S.\\,%2(page){%1(source)}',
-    --- \parencite [ postnote ]{ key }
-    ['textcite']        = '\\textcite[S.\\,%2(page)]{%1(source)}',   
-    
+    ['cite']        = '\\cite[%2(S_wenn_f._o._Anm.)]{%1(source)}',
+    ['parencite']        = '\\parencite[%2(S_wenn_f._o._Anm.)]{%1(source)}',
+    ['textcite']        = '\\textcite[%2(S_wenn_f._o._Anm.)]{%1(source)}',   
     --- \citeauthor{key}
     ['aut']             = '\\citeauthor{%0}',
     --- aut as a dynamic snippet
@@ -182,7 +217,7 @@ snippets.latex = {
     --- \citereset{}
     ['citereset']       = '\\citereset{%0}',
     ['cr']              = '\\citereset{%0}',
--- Some abbreviations frequently used in German academia
+    -- Some abbreviations frequently used in German academia
     ['oä']              = 'o.\\,ä. ',
     ['uä']              = 'u.\\,ä. ',
     ['ua']              = 'u.\\,a. ',
@@ -192,9 +227,7 @@ snippets.latex = {
     ['uu']              = 'u.\\,U. ',
     ['me']              = 'm.\\,E. ',
     ['mE']              = 'm.\\,E. ',
-    --- one of them usually followed by a citation.
-    ['vgl']             = '(vgl. \\cite[S.\\,%2(page)]{%1(source)})',
-
+    
 -- Misc
     ['emph']            = '\\emph{%0}',
     ['bf']              = '\\textbf{%0}',
@@ -202,8 +235,47 @@ snippets.latex = {
     ['fn']              = '\\footnote{%0}',
     ['ma']              = '\\marginpar{%0}',
     ['rn']              = '\\marginpar{%0}',
+    ['secframe']        = '\\sectionframe{}\n%0',
     -- a Comment Box I use in my excerpts
     ['anm']             = '\\begin{mdframed}[backgroundcolor=gray!20,roundcorner=8pt]\n\t\\textbf{Anm. ' .. dynsn.fields.user_initials .. ':} %0\n\\end{mdframed}',
+    -- a horizontal line
+    ['line']            = '\\hrulefill\n%0',
     -- current_author ist defined in dynamic snippets. It's usually a bibtex key
     ['ca']      = dynsn.fields.current_author,
+}
+
+snippets.rust = {
+    ['p']           = 'println!("%0");',
+    ['fn']          = "fn %1(name)(%2(&self)) %3(-> %4(type) ){%0}\n// TODO test this",
+    ['ttt']         = '// TODO test this',
+}
+
+--- ########################################################################
+--- # Key Chains
+--- ########################################################################
+
+keys['c1'] = {
+    -- Geman gendering
+    g = function() buffer.add_text("\\_innen") end,
+    -- vulgar fractions
+    ['2'] = function() buffer.add_text("½") end,
+    ['3'] = function() buffer.add_text("⅓") end,
+    ['4'] = function() buffer.add_text("¼") end,
+    ['5'] = function() buffer.add_text("⅕") end,
+    ['6'] = function() buffer.add_text("⅙") end,
+}
+
+keys['c2'] = {
+    -- German Quotation marks
+    up = function() buffer.add_text("“") end,
+    down = function() buffer.add_text("„") end,
+    left = function() buffer.add_text("»") end,
+    right = function() buffer.add_text("«") end,
+    -- vulgar fractions
+    ['3'] = function() buffer.add_text("⅔") end, 
+}
+
+keys['c3'] = {
+    -- vulgar fractions
+    ['4'] = function() buffer.add_text("¾") end,
 }
